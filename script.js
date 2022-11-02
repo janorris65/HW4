4fd6691fea3a2aab25f40a2e43bdfac47ca49290
Joshua <janorris65@gmail.com>
2022-11-01 23:13:16 -0500

    tidy code, create question block, display block, answercheck block

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 const buttons = document.querySelectorAll("button");
 const startBtn = document.getElementById("StartBtn");
 const INTERVAL = 1000;
-const TIMER = 10;
+let TIMER = 10;
 const digitalTimer = document.getElementById("countdown");
 let numlog = 0;
-//Start button functionality includes 1st Q, timer and numlog to advance q
+
+//Start button functionality displays 1st Question 'displayFunction, timer start 'countDown' and 'numlog' to advance q
 startBtn.addEventListener("click", function (event) {
   startBtn.textContent = "";
   numlog = 1;
-  var elQuestion = document.getElementById("Question");
-  elQuestion.textContent = overHead[1].question;
-
-  var eloption1 = document.getElementById("option1");
-  eloption1.textContent = overHead[1].options[0];
-
-  var eloption2 = document.getElementById("option2");
-  eloption2.textContent = overHead[1].options[1];
-
-  var eloption3 = document.getElementById("option3");
-  eloption3.textContent = overHead[1].options[2];
-
-  var eloption4 = document.getElementById("option4");
-  eloption4.textContent = overHead[1].options[3];
-
-  function countDown() {
-    let i = TIMER;
-    const timerdown = setInterval(function () {
-      digitalTimer.textContent = i;
-      i--;
-      if (i < 0) {
-        clearInterval(timerdown);
-      }
-    }, INTERVAL);
-  }
+  displayFunction(numlog);
   countDown();
 });
-const Q0 = {
-  //number: "Start",
-  question: "",
-  options: "",
-  answer: "",
-};
+// code block to check answer
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    if (event.target.textContent === overHead[numlog].answer) {
+    }
+  });
+});
+//code block for timer
+function countDown() {
+  let i = TIMER;
+  const timerdown = setInterval(function () {
+    digitalTimer.textContent = i;
+    i--;
+    if (i < 0) {
+      clearInterval(timerdown);
+    }
+  }, INTERVAL);
+}
+//code block for display function
+function displayFunction(x) {
+  var elQuestion = document.getElementById("Question");
+  elQuestion.textContent = overHead[x].question;
 
-const Q1 = {
-  //number: 1,
-  question: "Did George Washington like you?",
-  options: ["yes", "no", "maybe", "on good days"],
-  answer: "no",
-};
+  var eloption1 = document.getElementById("option1");
+  eloption1.textContent = overHead[x].options[0];
 
-const Q2 = {
-  //number: 2,
-  question: "Did John Adams like you?",
-  options: ["yes", "no", "maybe", "on good days"],
-  answer: "no",
-};
-let overHead = [Q0, Q1, Q2];
+  var eloption2 = document.getElementById("option2");
+  eloption2.textContent = overHead[x].options[1];
+
+  var eloption3 = document.getElementById("option3");
+  eloption3.textContent = overHead[x].options[2];
+
+  var eloption4 = document.getElementById("option4");
+  eloption4.textContent = overHead[x].options[3];
+}
+
+// code block to create questions
+function questionCreator(question, options, answer) {
+  this.question = question;
+  this.options = options;
+  this.answer = answer;
+}
+
+const Q0 = new questionCreator("", "", "");
+const Q1 = new questionCreator(
+  "Did George Washington like you?",
+  ["yes", "no", "maybe", "on good days"],
+  "no"
+);
+const Q2 = new questionCreator(
+  "Did John Adams like you?",
+  ["yes", "no", "maybe", "on good days"],
+  "no"
+);
+const Q3 = new questionCreator(
+  "Did Thomas Jefferson like you?",
+  ["yes", "no", "maybe", "on good days"],
+  "no"
+);
+let overHead = [Q0, Q1, Q2, Q3]; //array for question storage
+
 // Code Block for advancing questions with each click
-
 buttons.forEach(function (button) {
   button.addEventListener("click", function (event) {
     let x = numlog;
@@ -66,39 +82,9 @@ buttons.forEach(function (button) {
       numlog = x;
     }
     console.log(numlog);
-    var elQuestion = document.getElementById("Question");
-    elQuestion.textContent = overHead[numlog].question;
-
-    var eloption1 = document.getElementById("option1");
-    eloption1.textContent = overHead[numlog].options[0];
-
-    var eloption2 = document.getElementById("option2");
-    eloption2.textContent = overHead[numlog].options[1];
-
-    var eloption3 = document.getElementById("option3");
-    eloption3.textContent = overHead[numlog].options[2];
-
-    var eloption4 = document.getElementById("option4");
-    eloption4.textContent = overHead[numlog].options[3];
+    displayFunction(numlog);
   });
 });
 
-/*var elnumber = document.getElementById("StartBtn");
-elnumber.textContent = overHead[1].number;*/
-
-/*var elQuestion = document.getElementById("Question");
-elQuestion.textContent = overHead[numlog].question;
-
-var eloption1 = document.getElementById("option1");
-eloption1.textContent = overHead[numlog].options[0];
-
-var eloption2 = document.getElementById("option2");
-eloption2.textContent = overHead[numlog].options[1];
-
-var eloption3 = document.getElementById("option3");
-eloption3.textContent = overHead[numlog].options[2];
-
-var eloption4 = document.getElementById("option4");
-eloption4.textContent = overHead[numlog].options[3];*/
-
-// make a question maker form
+// TODO make a question maker form
+// TODO make timer subtracting from wrong answers
